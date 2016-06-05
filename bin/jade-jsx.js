c98883427d2e08e35c415d0b58ce7fc538d56264
yml <yuemenglong@126.com>
2016-06-05 16:13:41 +0800

    fix path not required remove out in cmd line, use > instead

--- bin/jade-jsx.js
+++ bin/jade-jsx.js
@@ -2,6 +2,7 @@
 
 var jadeToJsx = require("..");
 var fs = require("fs");
+var path = require("path");
 
 function getAbsoluteDir() {
     var p = path.resolve(process.cwd(), process.argv[2]);
@@ -10,16 +11,12 @@ function getAbsoluteDir() {
 }
 
 if (require.main == module) {
-    if (process.argv.length < 3) {
-        console.log("Usage: jade-jsx in [out]");
+    if (process.argv.length !== 3) {
+        console.log("Usage: jade-jsx <file>");
         return;
     }
     var inFile = process.argv[2];
     var src = fs.readFileSync(inFile).toString();
     var jsx = jadeToJsx(src, "jade", getAbsoluteDir());
     console.log(jsx);
-    var outFile = process.argv[3];
-    if (outFile) {
-        fs.writeFileSync(outFile, jsx);
-    }
 }
